21529d2db9a3a4560338adbe60e5204ab8902254
jacobfriis <jacob.friis@simplecomplex.net>
2019-03-16 12:29:31 +0100

    ValidationFailureException support more fine-grained retrieval of failures.

--- src/Exception/ValidationFailureException.php
+++ src/Exception/ValidationFailureException.php
@@ -34,10 +34,55 @@ class ValidationFailureException extends UserMessageException
     }
 
     /**
+     * Get list of validation failures.
+     *
+     * Skipping empty values may be relevant when keys are column names
+     * (correlating to HTTP request properties) and values are messages meant
+     * for a HTTP response header.
+     *
+     * @param bool $skipEmptyValues
+     *      True: skip items whose bucket value is empty.
+     *
      * @return array
      */
-    public function getFailures() : array
+    public function getFailures(bool $skipEmptyValues = false) : array
     {
-        return $this->failures ?? [];
+        if ($this->failures) {
+            if ($skipEmptyValues) {
+                $net = [];
+                foreach ($this->failures as $key => $val) {
+                    if ($val) {
+                        $net[$key] = $val;
+                    }
+                }
+                return $net;
+            }
+            return $this->failures;
+        }
+        return [];
+    }
+
+    /**
+     * Get keys of failures, excluding keys that are numeric.
+     *
+     * Getting names only may be relevant when keys are column names
+     * (correlating to HTTP request properties) and values are messages meant
+     * for a HTTP response header.
+     *
+     * @return string[]
+     */
+    public function getFailureNames() : array
+    {
+        if ($this->failures) {
+            $names = array_keys($this->failures);
+            $net = [];
+            foreach ($names as $name) {
+                if ($name && !ctype_digit($name)) {
+                    $net[] = $name;
+                }
+            }
+            return $net;
+        }
+        return [];
     }
 }
